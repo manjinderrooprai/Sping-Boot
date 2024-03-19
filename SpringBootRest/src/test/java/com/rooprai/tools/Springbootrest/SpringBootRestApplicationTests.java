b27549c303c03911b46f9fbcc1ebe4ee43d14045
Manjinder Singh Rooprai <rooprai0044@gmail.com>
2024-03-19 01:07:18 -0700

    name changes

---
+++ SpringBootRest/src/test/java/com/rooprai/tools/Springbootrest/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rooprai.tools.Springbootrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
