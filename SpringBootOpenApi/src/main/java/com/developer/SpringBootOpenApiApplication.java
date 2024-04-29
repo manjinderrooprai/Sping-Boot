5c76ecb492fa41277bb273d7b81a3ae2f9fdfd06
Manjinder Singh Rooprai <rooprai0044@gmail.com>
2024-04-29 01:37:30 -0700

    Experiments project: open api

---
+++ SpringBootOpenApi/src/main/java/com/developer/SpringBootOpenApiApplication.java
@@ -0,0 +1,16 @@
+package com.developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Manjinder.rooprai
+ */
+@SpringBootApplication
+public class SpringBootOpenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOpenApiApplication.class, args);
+	}
+
+}
